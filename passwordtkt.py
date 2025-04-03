de8394560c129e8582cb8b7f90284416e179badf
theNexus107 <swastikchatterjee2006@gmail.com>
2025-04-03 13:09:12 +0530

    passwdgnrtor

---
+++ passwordtkt.py
@@ -0,0 +1,40 @@
+import random
+import tkinter as tk
+
+def generate_password():
+    try:
+        length = int(length_entry.get())  
+        if length < 1:
+            password_var.set("Length must be greater than 0")
+            return
+        characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        password = ""
+        
+        for i in range(length):  
+            password += random.choice(characters)
+        
+        password_var.set(password)  
+    except ValueError:
+        password_var.set("Enter a valid number")
+
+
+root = tk.Tk()
+root.title("Password Generator")
+root.geometry("300x200")
+
+
+tk.Label(root, text="Enter Password Length:").pack(pady=5)
+length_entry = tk.Entry(root)
+length_entry.pack(pady=5)
+
+
+generate_button = tk.Button(root, text="Generate Password", command=generate_password)
+generate_button.pack(pady=10)
+
+
+password_var = tk.StringVar()
+password_label = tk.Label(root, textvariable=password_var, font=("Arial", 14))
+password_label.pack(pady=10)
+
+
+root.mainloop()
